59b95c664de3c886f03d567b385034eb8ba9cfba
MayankGeek <msharma.onetriple9@gmail.com>
2020-10-02 23:23:03 +0530

    Created a new folder and added a program

---
+++ c++ basic programs/add_two_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, add;
+    //Taking input
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin>> num2;
+
+    // adding the numbers stored in the variables
+    add = num1 + num2;
+
+    // Printing result
+    cout << num1 << " + " << num2 << " = " << add;     
+}
